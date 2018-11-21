3673016bd433fe5b93f2300b6ef5256f1e7dc696
a503-03 <shen_aihua0126@naver.com>
2018-11-21 12:20:23 +0900

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by 503-03 on 21/11/2018.
+//  Copyright © 2018 shenah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
